175f7dd5624c0c1957c95547bf75b1636dc093f0
essentiel2021 <anguijathi@gmail.com>
2022-10-06 18:35:45 +0000

    installation de laravel ui et modification du template login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SuccursaleController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,7 @@ Route::get('/', function () {
 });
 
 Route::resource('succursales',SuccursaleController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
